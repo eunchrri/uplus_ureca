e4ff1f7b570406a6d31fde6d11faab11ee901cf2
EUNCHAE LEE <leesilver7@gmail.com>
2025-04-08 09:40:57 +0900

    FEAT: day05_hook/05_customhook 실습 자료 업로드

--- react/day05_hook/05_customhook/src/Info.js
+++ react/day05_hook/05_customhook/src/Info.js
@@ -1,19 +1,23 @@
 import React from "react";
+import useInput from "./useInputs";
 
 const Info = () => {
-  
+  const [state, onChange, reset] = useInput({ name: "", nickname: "" });
+
+  // 구조 분해 할당
+  const { name, nickname } = state;
   return (
     <div>
       <div>
-        <input name="name" />
-        <input name="nickname"/>
-        <button>확인</button>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+        <button onClick={reset}>확인</button>
       </div>
       <div>
-        <b>이름:</b> 
+        <b>이름: {name}</b>
       </div>
       <div>
-        <b>닉네임: </b> 
+        <b>닉네임: {nickname}</b>
       </div>
     </div>
   );
