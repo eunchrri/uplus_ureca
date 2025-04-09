bda0e1b406b68143ea89897b0bff9514da919c8c
EUNCHAE LEE <leesilver7@gmail.com>
2025-04-09 09:15:52 +0900

    FEAT: 250409 자료 업로드

---
+++ react/day07_todo/01_todo-app-scss/src/components/TodoInsert.js
@@ -0,0 +1,6 @@
+// <form className="TodoInsert" onSubmit={onSubmit}>
+//   <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+//   <button type="submit">
+//     <MdAdd />
+//   </button>
+// </form>
---
+++ react/day07_todo/01_todo-app-scss/src/components/TodoListItem.js
@@ -0,0 +1,12 @@
+import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from "react-icons/md";
+import cn from "classnames";
+
+// <div className="TodoListItem">
+//   <div className={cn("checkbox", { checked })} >
+
+//     <div className="text"></div>
+//   </div>
+//   <div className="remove" >
+//     <MdRemoveCircleOutline />
+//   </div>
+// </div>
