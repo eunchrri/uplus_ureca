3092d84f6fa3725caeda25e7553c04e3c60f8006
EUNCHAE LEE <leesilver7@gmail.com>
2025-04-17 10:34:08 +0900

    FEAT: DAY13_AXIOS_SERVER\04_next_server 실습 업로드

--- react/day13_axios_server/04_next_server/src/utils/http-commons.js
+++ react/day13_axios_server/04_next_server/src/utils/http-commons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/eureka/";
+const BASE_URL = "http://10.4.2.100:8080/eureka/";
 
 // local vue api axios instance
 function localAxios() {
